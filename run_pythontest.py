d5018ada1cda706c07e7ae565478148294f86dc8
aki <aki@emptyroom.sakura.ne.jp>
2013-07-25 01:37:31 +0900

    add pep8 and pylint test

--- run_pythontest.py
+++ run_pythontest.py
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+pep8 pythontest.py > pep8.log
+pylint pythontest.py --output-format=parseable --report=yes > pylint.log
 nosetests pythontest.py --with-xunit --with-coverage --cover-branches
 coverage xml --omit='/usr/*' -o coverage.xml
